de0c567216bdf0cd7301a789c8774663a4cf5b36
ophyros <ophyros@gmail.com>
2015-10-19 14:46:32 +0300

    история комманд

--- app/components/ConsoleInput.js
+++ app/components/ConsoleInput.js
@@ -2,24 +2,54 @@ import React, { Component } from 'react';
 import '../styles/console-input.styl';
 
 class ConsoleInput extends Component {
+  constructor() {
+    super();
+    this.history = [];
+    this.historyIndex = 0;
+  }
+  
+  handleKeyPress(e) {
+    if (e.key === 'ArrowUp') {
+      this.historyIndex--;
+      if (this.historyIndex < 0) {
+        this.historyIndex = 0;
+        return;
+      } else {
+        this.refs.command.value = this.history[this.historyIndex];
+      }
+    } else if (e.key === 'ArrowDown') {
+      this.historyIndex++;
+      if (this.historyIndex >= this.history.length) {
+        this.historyIndex = this.history.length;
+        return;
+      } else {
+        this.refs.command.value = this.history[this.historyIndex];
+      }
+    }
+  }
 
   handleSubmit(e) {
     e.preventDefault();
     var command = this.refs.command.value.trim();
     if (!command) return;
     this.props.onCommandSubmit(command); // команда уходит в таб на исполнение
-    // тут наверно надо положить команду в историю
+    this.historyIndex = this.history.push(command);
+    if (this.historyIndex > 10) {
+      this.history.shift();
+      this.historyIndex--;
+    }
     this.refs.command.value = '';
-    return;
+    console.log(this.history);
+    console.log(this.historyIndex);
   }
 
   render() {
     return (
-      <form className="console-form" onSubmit={this.handleSubmit.bind(this)}>
+      <form className="console-form" onSubmit={this.handleSubmit.bind(this)} onKeyDown={this.handleKeyPress.bind(this)}>
         <input className="console-input" type="text" placeholder="Enter command" ref="command" />
         <input className="console-submit" type="submit" />
       </form>
-    );
+    )
   }
 }
 
