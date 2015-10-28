2c55016a00199e61ec8332b91c1793a5fd32298c
ophyros <ophyros@gmail.com>
2015-10-28 14:48:49 +0300

    автоскролл окна с результатами

--- app/components/ConsoleOutput.js
+++ app/components/ConsoleOutput.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import '../styles/console-output.styl';
 
 class ConsoleOutput extends Component {
+  componentDidUpdate() {
+    let node = ReactDOM.findDOMNode(this);
+    node.scrollTop = node.scrollHeight;
+  }
   render() {
     let text = this.props.tab.output.map((line, index) => {
           return (
